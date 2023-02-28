029efdf96e5adf905103abc5a006cc25ef8ddbef
emsa16 <emil.hietanen@gmail.com>
2023-02-28 09:13:26 +0100

    Add publishing endpoint

--- api/send-message/index.ts
+++ api/send-message/index.ts
@@ -3,24 +3,28 @@ import * as Ably from "ably/promises";
 import { HandlerEvent, HandlerContext } from "@netlify/functions";
 import { HEADERS } from "../../utils/constants";
 
+const CHANNEL = 'getting-started';
+const EVENT = 'data';
+  
 dotenv.config();
 
 export async function handler(event: HandlerEvent, _: HandlerContext) {
-  if (!process.env.ABLY_API_KEY_SUBSCRIBE) {
+  if (!process.env.ABLY_API_KEY_PUBLISH) {
     return {
       statusCode: 500,
       headers: HEADERS,
-      body: JSON.stringify(`Server missing ABLY_API_KEY_SUBSCRIBE`)
+      body: JSON.stringify(`Server missing ABLY_API_KEY_PUBLISH`)
     }
   }
 
   const clientId = event.queryStringParameters?.["clientId"] || process.env.DEFAULT_CLIENT_ID || "NO_CLIENT_ID"; // TODO implement or remove
-  const client = new Ably.Rest(process.env.ABLY_API_KEY_SUBSCRIBE);
-  const tokenRequestData = await client.auth.createTokenRequest({ clientId: clientId });
+  const ably = new Ably.Realtime(process.env.ABLY_API_KEY_PUBLISH);
+  const channel = ably.channels.get(CHANNEL);
+  channel.publish(EVENT, {message: 'this is published to everyone'});
 
   return {
     statusCode: 200,
     headers: HEADERS,
-    body: JSON.stringify(tokenRequestData)
+    body: JSON.stringify({message: 'ok'})
   };
 }
---
+++ utils/constants.ts
@@ -0,0 +1,4 @@
+export const HEADERS = {
+    'Access-Control-Allow-Origin': '*', // TODO restrict
+    'Content-Type': 'application/json'
+};
